0a8d279b729a3c2ff32442c2f8acda857044cbcd
cgj245 <1373815630@qq.com>
2018-07-25 18:50:01 +0800

    7.25

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -10,7 +10,7 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="/js/jquery-3.2.1.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/js/bootstrap.js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
 
     <!--引入CSS-->
     <link rel="stylesheet" type="text/css" href="/webuploader/webuploader.css">
